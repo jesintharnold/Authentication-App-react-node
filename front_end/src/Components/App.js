3febb036f4f11a80802ec593f268762372a99a47
Craxy_Jesi <39983328+jesintharnold@users.noreply.github.com>
2021-08-21 20:07:23 +0530

    Header -  Feature added

--- front_end/src/Components/App.js
+++ front_end/src/Components/App.js
@@ -1,10 +1,12 @@
 import '../Styles/App.scss';
 import {Register,Login} from './Login/Login';
 import {PersonalInfo,PersonalInfoEdit} from './PersonalInfo';
+import {Head} from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Head/>
     <PersonalInfoEdit/>
     <div className="foot">
      <span> JesinthArnold</span>
--- front_end/src/Components/Header.js
+++ front_end/src/Components/Header.js
@@ -1,12 +1,39 @@
+import DevSvg from "../Assets/devchallenges.svg";
+import DevSvg_lg from "../Assets/devchallenges-light.svg";
+import "../Styles/Head.scss";
+import {useState} from 'react';
 
-import DevSVG from '../../Assets/devchallenges.svg';
-const Head=()=>{
+export const Head=()=>{
+
+    const [toggle,setToggle]=useState(false);
+
 return(
     <div className="Head">
-        <img src={DevSVG} alt="Not Found"/>
-        </div>
+        <img src={DevSvg} alt="Not Found"/>
+         <div className="name__plate" onClick={()=>setToggle(!toggle)}>
+           <img src="https://unsplash.com/photos/x_8oJhYU31k/download?force=true&w=640"/>
+           <div className="Name">Xanthe Neal</div>
+           <i  className={toggle?'uis uis-triangle triangle  option__active':'uis uis-triangle triangle'}></i> 
+
+           <div className={toggle?'options__plate  option__active':'options__plate'}>
+             <div className="profile">
+                 <i class="uil uil-user-circle"></i>
+                 <span>My Profile</span>
+                 </div>
+             <div className="chat">
+             <i class="uil uil-users-alt"></i>
+             <span>Group Chat</span>
+             </div>
+             <div className="break"></div>
+             <button className="logout">
+             <i class="uil uil-signout"></i>
+             <span>Logout</span>
+             </button>
+         </div>
+         </div>
+         
+    </div>
 
 )
 };
 
-export default Head;
